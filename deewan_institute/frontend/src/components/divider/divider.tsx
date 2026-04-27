41fb051da50af08ca20dea5c583481cdbbbc38ae
Deewan Institute <app@deewaninstitute.com>
2026-04-27 09:27:24 +0300

    Qutaibh Renamed folder to lowercase via temp

--- deewan_institute/frontend/src/components/divider/divider.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./divider.module.scss";
-import "bootstrap";
-
-function Divider() {
-    return (
-        <hr className={`${styles.featuretteDivider} mx-auto my-4 scroll-section`} />
-    )
-}
-export default Divider;
