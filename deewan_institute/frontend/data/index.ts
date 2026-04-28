5d5b40d02ab8a647ff967e6ba87efacb73c84076
Deewan Institute <app@deewaninstitute.com>
2026-04-28 23:27:38 +0300

    changes

--- deewan_institute/frontend/data/index.ts
+++ deewan_institute/frontend/data/index.ts
@@ -1,10 +1,10 @@
-export { mainBooks }           from './mainbooks';
-export { ammiyehCollection }   from './ammiyehcollection';
-export { fushaBook }           from './fushabook';
-export { podcastSeasons }      from './podcastseasons';
+export { mainBooks }           from './mainBooks';
+export { ammiyehCollection }   from './ammiyehCollection';
+export { fushaBook }           from './fushaBook';
+export { podcastSeasons }      from './podcastSeasons';
 
 // Type exports
-export type { MainBook, Review }    from './mainbooks';
-export type { CollectionBook }      from './ammiyehcollection';
-export type { FushaBook }           from './fushabook';
-export type { PodcastSeason }       from './podcastseasons';
+export type { MainBook, Review }    from './mainBooks';
+export type { CollectionBook }      from './ammiyehCollection';
+export type { FushaBook }           from './fushaBook';
+export type { PodcastSeason }       from './podcastSeasons';
