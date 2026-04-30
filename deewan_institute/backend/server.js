7ef22efa30f0486adf845e4dce94a4c521aebabf
Deewan Institute <app@deewaninstitute.com>
2026-04-30 17:35:06 +0300

    changes

--- deewan_institute/backend/server.js
+++ deewan_institute/backend/server.js
@@ -48,8 +48,8 @@ const upload = multer({
 // With this:
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 578,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
