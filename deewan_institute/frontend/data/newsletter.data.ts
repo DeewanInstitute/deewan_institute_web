c11761166eaf88b23267c877e753d76e8aec3b5f
Deewan Institute <app@deewaninstitute.com>
2026-06-30 11:14:35 +0300

    Qutaibh Subeh - 30/06/2026 - Added the Newsletter

--- deewan_institute/frontend/data/newsletter.data.ts
+++ deewan_institute/frontend/data/newsletter.data.ts
@@ -11,6 +11,20 @@ export type NewsletterEdition = {
 };
 
 export const EDITIONS: NewsletterEdition[] = [
+
+  {
+    id: "july",
+    label: "July 2026",
+    tag: "Latest Issue",
+    title: "July Deewan Newsletter",
+    format: "rectangular",
+    aspectRatio: 56.25,
+    src: "https://www.canva.com/design/DAHN2j4SP-s/3joS_FTSBteJea8n76rv3A/view?embed",
+    href: "https://www.canva.com/design/DAHN2j4SP-s/3joS_FTSBteJea8n76rv3A/view",
+    description:
+      "Dive into Arabic language milestones, cultural highlights from Al-Weibdeh, and upcoming events at Deewan Institute.",
+
+  },
    {
     id: "June",
     label: "June 2026",
@@ -26,7 +40,7 @@ export const EDITIONS: NewsletterEdition[] = [
   {
     id: "may",
     label: "May 2026",
-    tag: "Latest Issue",
+    tag: "Previous Issue",
     title: "May Deewan Newsletter",
     format: "rectangular",
     aspectRatio: 56.25,
